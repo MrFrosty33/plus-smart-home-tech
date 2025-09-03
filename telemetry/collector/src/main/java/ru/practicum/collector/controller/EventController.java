2f5953ccf0c4c4936856fadf60bf9f3e68a94cb9
Frosty-WinPC <al.korobko@yahoo.com>
2025-09-03 19:24:46 +0200

    fix: пробросил порты, изменил уровень логов на Trace

--- telemetry/collector/src/main/java/ru/practicum/collector/controller/EventController.java
+++ telemetry/collector/src/main/java/ru/practicum/collector/controller/EventController.java
@@ -3,10 +3,9 @@ package ru.practicum.collector.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.collector.model.hub.HubEvent;
 import ru.practicum.collector.model.sensor.SensorEvent;
 import ru.practicum.collector.service.EventService;
@@ -20,13 +19,15 @@ public class EventController {
     private final EventService eventService;
     private final String className = this.getClass().getSimpleName();
 
-    @RequestMapping("/sensors")
+    @PostMapping("/sensors")
+    @ResponseStatus(HttpStatus.OK)
     public void receiveSensorEvent(@RequestBody @Valid SensorEvent sensorEvent) {
         log.trace("{}: received SensorEvent: {}", className, sensorEvent);
         eventService.produceSensorData(sensorEvent);
     }
 
-    @RequestMapping("/hubs")
+    @PostMapping("/hubs")
+    @ResponseStatus(HttpStatus.OK)
     public void receiveHubEvent(@RequestBody @Valid HubEvent hubEvent) {
         log.trace("{}: received HubEvent: {}", className, hubEvent);
         eventService.produceHubData(hubEvent);
