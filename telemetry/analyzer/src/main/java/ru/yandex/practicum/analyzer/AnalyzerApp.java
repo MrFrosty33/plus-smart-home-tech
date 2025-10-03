6f9d074323821c37c95a63ce791bce23cc57443b
Frosty-WinPC <al.korobko@yahoo.com>
2025-10-03 16:16:59 +0200

    fix: исправлена аннотация для поиска конфигураций AnalyzerApp.java

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApp.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerApp.java
@@ -2,10 +2,10 @@ package ru.yandex.practicum.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationProperties
+@ConfigurationPropertiesScan
 public class AnalyzerApp {
     public static void main(String[] args) {
         SpringApplication.run(AnalyzerApp.class, args);
