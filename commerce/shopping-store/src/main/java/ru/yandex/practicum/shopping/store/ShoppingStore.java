d22a37bd467e084b6adf12f02258dd00d5b3b1c5
Frosty-WinPC <al.korobko@yahoo.com>
2025-10-24 18:24:27 +0200

    add: базовые настройки конфигов для будущих модулей на config-server

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping/store/ShoppingStore.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping/store/ShoppingStore.java
@@ -2,16 +2,16 @@ package ru.yandex.practicum.shopping.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@ConfigurationPropertiesScan
 public class ShoppingStore {
     //todo конфигурация actuator в application.yml / в конфиге на config-server
     // management.endpoints...
-
-    //todo подключить cache: @Cacheable (для добавления), @CachePut (для обновления), @CacheEvict (для удаления)
-    // использовать можно Caffeine, настроить его и ограничить хранение в кэше по времени
+    // а также правильная ссылка на БД
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStore.class, args);
     }
--- commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping/store/service/ProductServiceImpl.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/shopping/store/service/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.dto.ProductCategory;
 import ru.yandex.practicum.dto.ProductDto;
+import ru.yandex.practicum.logging.Loggable;
 import ru.yandex.practicum.shopping.store.model.Pageable;
 import ru.yandex.practicum.shopping.store.model.SetProductQuantityStateRequest;
 import ru.yandex.practicum.shopping.store.repository.ProductRepository;
@@ -15,7 +16,11 @@ import java.util.List;
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
 
+    //todo подключить cache: @Cacheable (для добавления), @CachePut (для обновления), @CacheEvict (для удаления)
+    // использовать можно Caffeine, настроить его и ограничить хранение в кэше по времени
+    // @Loggable
     @Override
+    @Loggable
     public List<ProductDto> getWithPagination(ProductCategory category, Pageable pageable) {
         return List.of();
     }
