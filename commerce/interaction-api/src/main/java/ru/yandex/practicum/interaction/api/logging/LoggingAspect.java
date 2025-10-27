c4edc414065217d41e532ca80c7146a45814d7f2
Frosty-WinPC <al.korobko@yahoo.com>
2025-10-27 21:46:11 +0100

    add: добавил spring-aop, spring-context зависимости. аспект всё одно не работает

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/logging/LoggingAspect.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/logging/LoggingAspect.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.interaction.api.logging;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -14,21 +12,18 @@ import java.util.Arrays;
 
 @Aspect
 @Component
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@Slf4j
 public class LoggingAspect {
-
-    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
-
     //todo не работает, надо разбираться. Пакеты уже поправил, добавил уровень логгирования для этого модуля
     // всё одно не работает
     @SuppressWarnings("unused")
     @Around("@annotation(ru.yandex.practicum.interaction.api.logging.Loggable)")
     public Object logWithExecutionTimeMeasure(ProceedingJoinPoint joinPoint) throws Throwable {
         String className = joinPoint.getTarget().getClass().getSimpleName();
-        logger.info("{}: Entering method: {}", className, joinPoint.getSignature());
+        log.info("{}: Entering method: {}", className, joinPoint.getSignature());
 
         //todo может как-то красивее выводить стоит
-        logger.info("{}: Request Parameters: {}", className, Arrays.toString(joinPoint.getArgs()));
+        log.info("{}: Request Parameters: {}", className, Arrays.toString(joinPoint.getArgs()));
 
         Instant beforeExecute = Instant.now();
         Object result = joinPoint.proceed();
@@ -36,7 +31,7 @@ public class LoggingAspect {
 
         long executionTimeMs = Duration.between(beforeExecute, afterExecute).toMillis();
 
-        logger.info("{}: Exiting method: {}, Executed in: {} ms, Response: {}",
+        log.info("{}: Exiting method: {}, Executed in: {} ms, Response: {}",
                 className, joinPoint.getSignature(), executionTimeMs, result);
 
         return result;
