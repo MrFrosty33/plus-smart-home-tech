949546f2a87e55cd0b47ac550d08ae07d5c1bb3a
Frosty-WinPC <al.korobko@yahoo.com>
2025-10-30 13:39:30 +0100

    add: нарочно крашу приложение, чтобы не висели тесты. Надеюсь, поможет

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApp.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/WarehouseApp.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.warehouse;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,8 +8,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication(scanBasePackages = {"ru.yandex.practicum.warehouse", "ru.yandex.practicum.interaction.api"})
 @EnableFeignClients(basePackages = "ru.yandex.practicum.interaction.api.feign")
 //@EnableCaching
-public class WarehouseApp {
+public class WarehouseApp implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        throw new IllegalStateException("пока что так, чтобы не расходовать 6ч времени тестов GitHub Actions");
     }
 }
