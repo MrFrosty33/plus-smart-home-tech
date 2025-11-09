9674edff5593617d09589f764e91bad839e3070a
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-09 16:02:44 +0100

    add: необходимые зависимости и базовая структура пакетов в новых модулях

---
+++ commerce/payment/src/main/java/ru/yandex/practicum/payment/PaymentApp.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = {"ru.yandex.practicum.payment", "ru.yandex.practicum.interaction.api"})
+@EnableFeignClients(basePackages = "ru.yandex.practicum.interaction.api.feign")
+public class PaymentApp {
+
+    //TODO
+    // в спецификациях есть неточности. В некоторых эндпоинтах можно запрашивать будет допольнительные параметры
+    // опционально, после реализации всего:
+    // cache
+    // Spring Security - накинуть на gateway, чтобы каждый запрос аутентифицировался, см. QA
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApp.class, args);
+    }
+}
