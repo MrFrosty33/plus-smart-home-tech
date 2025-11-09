70a0838f44283706fba0a5488148825e60bb05c3
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-09 21:55:09 +0100

    add: добавил новые таблицы commerce_warehouse в скрипт инициализации БД в compose.yml

--- db/schema.sql
+++ db/schema.sql
@@ -133,3 +133,18 @@ CREATE TABLE IF NOT EXISTS products(
                  CHECK (quantity >= 0)
 );
 
+CREATE TABLE IF NOT EXISTS order_bookings(
+    order_booking_id UUID PRIMARY KEY,
+    delivery_id UUID
+);
+
+CREATE TABLE IF NOT EXISTS products_order_bookings(
+    product_id UUID NOT NULL,
+    order_booking_id UUID NOT NULL,
+    quantity INTEGER,
+    PRIMARY KEY (product_id, order_booking_id),
+    FOREIGN KEY (order_booking_id) REFERENCES order_bookings(order_booking_id),
+    CONSTRAINT check_quantity
+        CHECK (quantity > 0)
+);
+
