8fc4e8fe57de8606bd3174ff6894f370f4f839c3
Frosty-WinPC <al.korobko@yahoo.com>
2025-11-10 14:53:26 +0100

    add: controller & service interface модуля delivery

--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/controller/DeliveryController.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/controller/DeliveryController.java
@@ -2,16 +2,47 @@ package ru.yandex.practicum.delivery.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.delivery.service.DeliveryService;
+import ru.yandex.practicum.interaction.api.dto.DeliveryDto;
+import ru.yandex.practicum.interaction.api.dto.OrderDto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/delivery")
 @RequiredArgsConstructor
 @Validated
 public class DeliveryController {
     private final DeliveryService deliveryService;
 
+    @PutMapping
+    public DeliveryDto create(@RequestBody DeliveryDto deliveryDto) {
+        return deliveryService.create(deliveryDto);
+    }
 
+    @PostMapping("/successful")
+    public void successful(@RequestBody UUID orderId) {
+        deliveryService.successful(orderId);
+    }
+
+    @PostMapping("/picked")
+    public void picked(@RequestBody UUID orderId) {
+        deliveryService.picked(orderId);
+    }
+
+    @PostMapping("/failed")
+    public void failed(@RequestBody UUID orderId) {
+        deliveryService.failed(orderId);
+    }
+
+    @PostMapping("/cost")
+    public BigDecimal calculateDeliveryCost(@RequestBody OrderDto orderDto) {
+        return deliveryService.calculateDeliveryCost(orderDto);
+    }
 }
--- commerce/delivery/src/main/java/ru/yandex/practicum/delivery/service/DeliveryService.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/delivery/service/DeliveryService.java
@@ -1,4 +1,19 @@
 package ru.yandex.practicum.delivery.service;
 
+import ru.yandex.practicum.interaction.api.dto.DeliveryDto;
+import ru.yandex.practicum.interaction.api.dto.OrderDto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public interface DeliveryService {
+    DeliveryDto create(DeliveryDto deliveryDto);
+
+    void successful(UUID orderId);
+
+    void picked(UUID orderId);
+
+    void failed(UUID orderId);
+
+    BigDecimal calculateDeliveryCost(OrderDto orderDto);
 }
